5e8134df394f380fc1308c98632732cdf49322c3
HeerKaisair <1767407822@qq.com>
2021-08-09 16:06:23 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+cd ../source/Amiable.Core
+# 进入源代码包
+cp -r Events ../../core-source/Amiable.Core/
+cp -r Service ../../core-source/Amiable.Core/
+cp -r Exports ../../core-source/Amiable.Core/
+# 复制三个文件夹
+cd ../../core-source
+
 git init
 git add -A
 git commit -m 'deploy'
+
+
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:heerheer/Amiable.git master:core
