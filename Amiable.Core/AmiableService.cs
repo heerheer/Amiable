52feea6d8aeb2b649155e912e63133ddbe1fb4ab
HeerKaisair <1767407822@qq.com>
2021-08-17 18:38:54 +0800

    deploy

--- Amiable.Core/AmiableService.cs
+++ Amiable.Core/AmiableService.cs
@@ -1,17 +1,20 @@
 ﻿using Amiable.Adapter.Kum;
 using Amiable.Adapter.MQ;
 using Amiable.SDK;
+using Amiable.SDK.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Amiable.Adapters.Kum;
-using Amiable.Adapters.MQ;
-using Amiable.Adapters.XQ;
 using Amiable.Adapters.LYP;
+using Amiable.Adapters.MQ;
 using Amiable.Adapters.Xlz;
+using Amiable.Adapters.XQ;
 
 namespace Amiable.Core
 {
     public static partial class AmiableService
     {
-
         /// <summary>
         /// 设置App信息
         /// </summary>
@@ -19,15 +22,27 @@ namespace Amiable.Core
         {
             App.AppInfo = new AppInfo
             {
-                Name = "Amiable.Core",
+                Name = "Amiable.Example",
                 Author = "Heer Kaisair",
                 Version = "1.0.0",
                 Description = "样例",
-                AppId = "top.amiable.core"
+                AppId = "top.amiable.example"
             };
+        }
 
+        /// <summary>
+        /// 在这里注册事件
+        /// </summary>
+        private static void RegEvents()
+        {
+            AddPluginEvent<Example.AmiableTest>();
+            AddPluginEvent<UITest.MenuEvent>();
+
+            //AddPluginEvent<IPluginEvent类型>();
 
         }
+
+
 
         /// <summary>
         /// 对AppService的建造
@@ -35,7 +50,6 @@ namespace Amiable.Core
         /// <param name="service"></param>
         public static void ServiceBuilder(AppService service)
         {
-            
             //添加对这些框架的API包装器
             service.AddMQConfig().AddKumConfig().AddXQConfig().AddLypConfig().AddXlzConfig();
         }
